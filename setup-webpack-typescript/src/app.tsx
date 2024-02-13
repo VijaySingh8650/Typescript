7b0d9659cbe27907afadd8f148150ea4cd8a3017
VijaySingh8650 <rathorevijay995@gmail.com>
2024-02-13 20:14:11 +0530

    changes

--- setup-webpack-typescript/src/app.tsx
+++ setup-webpack-typescript/src/app.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 import "./style/app.css";
-
+import logo from "./logo.svg";
 
 const App = () => {
-  
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className='App-link'>
-          React + TypeScript + Webpack
-        </p>
+        <p className="App-link">React + TypeScript + Webpack</p>
+        <img src={logo} alt="" />
       </header>
     </div>
-  )
-}
+  );
+};
 
 export default App;
